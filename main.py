343e156d120504864e8f5f041e561dbe2d897e4b
HazelSharma <99499352+HazelSharmaCoderHZ@users.noreply.github.com>
2024-09-30 23:17:33 +0530

    Update main.py hz

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import asyncio
 import os
-
 from discord.utils import setup_logging
-
 from core import Bot
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
-
+# Get the Discord bot token from environment variables
 token = os.environ.get("TOKEN")
 
-
 async def main() -> None:
+    # Set up logging for the bot
     setup_logging()
     async with Bot() as bot:
-        
+        # Start the bot and reconnect if disconnected
         await bot.start(token=token, reconnect=True)
 
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    if token is None:
+        print("Error: TOKEN environment variable not set.")
+    else:
+        asyncio.run(main())
